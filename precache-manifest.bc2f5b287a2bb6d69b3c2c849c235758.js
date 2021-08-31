4cc65dd95d5785ce96dcf1c040580f73c6d916ab
Polina Razumna <poliandiy@gmail.com>
2021-08-31 13:31:02 +0300

    Updates

---
+++ precache-manifest.bc2f5b287a2bb6d69b3c2c849c235758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147991b51f046949527a5882befaa134",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b5074e059f6044f0854",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5074e059f6044f0854",
+    "url": "./static/js/main.0585e01b.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
